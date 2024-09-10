0938537c4ca9a80ee1e64854d2bb101b26d9dc97
jaomilholo <joaomilholo12@gmail.com>
2024-09-09 21:17:35 -0300

    fundamento 003

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // arrays, objetos
-let metas = [2, "alô"] 
-console.log(metas[1] + ", " +  metas[0])
+// let metas = [2, "alô"] 
+// console.log(metas[1] + ", " +  metas[0])
 
 let meta = {
   value: 'Ler um livro por mês',
-  address: 2,
   checked: false,
-  log: (info) => {
-    console.log(info)
-  }
 }
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+  meta,
+  {
+    value: "Caminhar 20 minutos todos os dias",
+    checked: false,
+  }
+]
+
+console.log(metas[1].value)
 
 // function   // arrow function
 // const criarMeta = () => {}
