3656c053c080089e860f937070821908353ad932
jaomilholo <joaomilholo12@gmail.com>
2024-09-11 10:03:47 -0300

    resolve problemas marcadas/desmarcadas

--- index.js
+++ index.js
@@ -27,14 +27,16 @@ const listarMetas = async () => {
     instructions: false,
   })
 
-  if(respostas.length == 0) {
-    console.log('Nenhuma meta selecionada!b')
-    return
-  }
-
   metas.forEach((m) => {
     m.checked = false
   })
+
+  if(respostas.length == 0) {
+    console.log('Nenhuma meta selecionada! 😢')
+    return
+  }
+
+
 
   respostas.forEach((resposta) => {
     const meta = metas.find((m) => {
